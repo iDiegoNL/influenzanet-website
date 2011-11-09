57372ef6d4ba470b3d7dea433b68e2ce80e7756a
Emanuele Aina <emanuele.aina@dndg.it>
2011-11-09 14:43:56 +0100

    Do not add the 'gid' query param if already there

--- apps/pollster/views.py
+++ apps/pollster/views.py
@@ -12,7 +12,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from cms import settings as cms_settings
 from apps.survey.models import SurveyUser
 from . import models, forms, fields, parser, json
-import re, datetime, locale, csv
+import re, datetime, locale, csv, urlparse, urllib
 
 def request_render_to_response(req, *args, **kwargs):
     kwargs['context_instance'] = RequestContext(req)
@@ -166,7 +166,12 @@ def survey_run(request, shortname, next=None):
             form.save()
             next_url = next or _get_next_url(request, reverse(survey_run, kwargs={'shortname': shortname}))
             if global_id:
-                next_url = next_url + "?gid=" + global_id
+                # add or override the 'gid' query parameter
+                next_url_parts = list(urlparse.urlparse(next_url))
+                query = dict(urlparse.parse_qsl(next_url_parts[4]))
+                query.update({'gid': global_id})
+                next_url_parts[4] = urllib.urlencode(query)
+                next_url = urlparse.urlunparse(next_url_parts)
             return HttpResponseRedirect(next_url)
         else:
             survey.set_form(form)
