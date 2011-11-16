2b0dc32a535108fb8c0a4e313bdeb4a77281ff38
Klaas van Schelven <klaas@vanschelven.com>
2011-11-16 21:47:55 +0100

    Edit your settings

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.conf import settings
+from django.contrib.auth.models import User
 
 from registration.forms import RegistrationForm
+from apps.reminder.models import UserReminderInfo
 
 attrs_dict = {'class': 'required'}
 
@@ -22,3 +25,42 @@ class UnicodeUserCreationForm(UserCreationForm):
     username = forms.RegexField(label=_("Username"), max_length=30, regex=r'(?u)^[\w.@+-]+$',
         help_text = _("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
         error_messages = {'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+
+
+class MySettingsForm(forms.Form):
+    email = forms.EmailField(label=_("Email"))
+    send_reminders = forms.BooleanField(label=_("Send reminders"), help_text=_("Check this box if you wish to receive weekly reminders throughout the flu season"), required=False)
+    language = forms.ChoiceField(label=_("Language"), choices=settings.LANGUAGES)
+    
+    def __init__(self, *args, **kwargs):
+        self.instance = kwargs.pop('instance')
+        self.reminder_info, _ = UserReminderInfo.objects.get_or_create(user=self.instance, defaults={'active': True})
+
+        initial = kwargs.pop('initial', {})
+        initial['email'] = self.instance.email
+        initial['send_reminders'] = self.reminder_info.active
+        initial['language'] = self.reminder_info.language
+        kwargs['initial'] = initial
+
+        super(MySettingsForm, self).__init__(*args, **kwargs)
+        
+        if len(settings.LANGUAGES) == 1:
+            del self.fields['language']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.exclude(id=self.instance.id).filter(email=email).count():
+            raise forms.ValidationError(_("This email is already in use"))
+        return email
+
+    def save(self):
+        self.instance.email = self.cleaned_data['email']
+        self.reminder_info.active = self.cleaned_data['send_reminders']
+        
+        if 'language' in self.cleaned_data:
+            self.reminder_info.language = self.cleaned_data['language']
+
+        self.instance.save()
+        self.reminder_info.save()
+
+
