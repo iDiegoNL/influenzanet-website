ca1bf3774346e715928060f032b946028d429195
Clément Turbelin <clement.turbelin@gmail.com>
2011-12-27 17:24:19 +0100

    counter refactor

--- apps/count/management/commands/counter_fetch.py
+++ apps/count/management/commands/counter_fetch.py
@@ -5,17 +5,20 @@ from django.core.management.base import NoArgsCommand, BaseCommand
 
 
 class Command(NoArgsCommand):
-  # fetch all sites
-  data = dict([(country, site_fetch_count(country)) for country in SOURCES.keys() ])
-  
-  try:
-      cache_delay = settings.COUNT_CACHE_TIMOUT
-  except AttributeError:
-   cache_delay = 60 * 30 
-  
-  for country in SOURCES.keys():
-      key = "count-counter-%s" % country
-      cache.set(key, data[country], timeout=cache_delay)
+    def handle_noargs(self, **options):
+         
+        try:
+            cache_delay = settings.COUNT_CACHE_TIMOUT
+        except AttributeError:
+            cache_delay = 60 * 30 
 
-  cache.set("count-counter-all", data, timeout=cache_delay)
+        data = {}
+        for country in SOURCES.keys():
+            result = site_fetch_count(country)
+            data[country] = result
+            # set in cache
+            key = "count-counter-%s" % country
+            cache.set(key, result, timeout=cache_delay)
+
+        cache.set("count-counter-all", data, timeout=cache_delay)
   
