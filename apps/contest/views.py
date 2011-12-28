76590bd548867a916d2cf704c5f65e055a192d41
Klaas van Schelven <klaas@vanschelven.com>
2011-12-28 12:53:45 +0100

    Removed print statement

--- apps/contest/views.py
+++ apps/contest/views.py
@@ -14,8 +14,6 @@ def main(request):
     already_done = Prediction.objects.filter(user=request.user).count()
 
     MEDIA_URL = settings.MEDIA_URL
-    print 'wtf', MEDIA_URL
-
     if not already_done:
         return HttpResponse("""<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" 
 codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" 
