a19766ffcad456da1b84b7a767664cfb5c68e9a3
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-01-09 15:22:43 +0100

    syntax

--- apps/sw_feedback/management/commands/feedback_testmail.py
+++ apps/sw_feedback/management/commands/feedback_testmail.py
@@ -1,13 +1,10 @@
-recipientsfrom apps.count.sites import *
 from django.conf import settings
-from django.core.cache import cache
-from django.core.management.base import NoArgsCommand, BaseCommand
+from django.core.management.base import NoArgsCommand
 from django.core.mail import send_mail
 
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-       
-       recipients = [ x[1] for x in settings.ADMINS ]  
-       send_mail('Test', 'Test', settings.DEFAULT_FROM_EMAIL, recipients)
+        recipients = [ x[1] for x in settings.ADMINS ]
+        send_mail('Test', 'Test', settings.DEFAULT_FROM_EMAIL, recipients)
   
