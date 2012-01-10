c3eb7ba89ad8032e17f292812dc3d06073d579d2
Klaas van Schelven <klaas@vanschelven.com>
2012-01-10 17:29:28 +0100

    EpiDB client scripts: more general

--- epidb/client/dump_upload_load.bash
+++ epidb/client/dump_upload_load.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-cat dump_intake_2_countries.sql | psql
-cat dump_weekly_2_countries.sql | psql
+#cat dump_intake_2_countries.sql | psql
+#cat dump_weekly_2_countries.sql | psql
 
-#cat dump_intake.sql | psql
-#cat dump_weekly.sql | psql
+cat dump_intake.sql | psql
+cat dump_weekly.sql | psql
 
 pg_dump -t epidb_results_intake -t epidb_results_weekly --clean > epidb_results.sql
 grep -v 'ALTER TABLE public.*OWNER TO' epidb_results.sql.tmp
 mv epidb_results.sql.tmp epidb_results.sql
 
-scp epidb_results.sql ggm@toast:
+scp epidb_results.sql YOURUSER@85.90.70.27:
 
-ssh ggm@toast 'cat epidb_results.sql | psql'
+ssh YOURUSER@85.90.70.27 'cat epidb_results.sql | psql'
 
