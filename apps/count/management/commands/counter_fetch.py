128fab8ba7f57e4b2bcb61721bc18b753defc7f5
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-01-16 15:20:12 +0100

    counter fetch one country

--- apps/count/management/commands/counter_fetch.py
+++ apps/count/management/commands/counter_fetch.py
@@ -1,26 +1,40 @@
 from apps.count.sites import *
 from django.conf import settings
 from django.core.cache import cache
-from django.core.management.base import NoArgsCommand, BaseCommand
+from django.core.management.base import BaseCommand
+from optparse import make_option
 
+class Command(BaseCommand):
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
-         
+    help = 'Fetch counter value'
+    option_list = BaseCommand.option_list + (
+        make_option('-c', '--country', action='store_string',
+            dest='country', default='all',
+            help='Country to fetch.'),
+     )
+     
+    def handle(self, **options):
+        
+        country = options.get('country') 
         try:
             cache_delay = settings.COUNT_CACHE_TIMEOUT
         except AttributeError:
             cache_delay = 60 * 30
 
-        data = {}
-        for country in SOURCES.keys():
-            result = site_fetch_count(country)
-            data[country] = result
-            # set in cache
-            key = "count-counter-%s" % country
-            cache.set(key, result, timeout=cache_delay)
-            if(settings.DEBUG): 
-                print("%s=%s " %(country,result))
+        if country == 'all':
+            coutries = SOURCES.keys()
+        else:
+            coutries = [country]
         
-        cache.set("count-counter-all", data, timeout=cache_delay)
+        data = {}
+        for c in coutries:
+            result = site_fetch_count(c)
+            data[c] = result
+            # set in cache
+            cache_key = "count-counter-%s" % c
+            cache.set(cache_key, result, timeout=cache_delay)
+            if(settings.DEBUG): 
+                print("%s=%s " %(c, result))
+        
+            
   
