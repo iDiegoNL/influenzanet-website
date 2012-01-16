iDiegoNL/influenzanet-website

commit 59d11efee8a014eb018a0a803bdf21f677b8e940

adding geometry table as a chart's field. List of tables in settings.py

apps/pollster/models.py
@@ -861,7 +861,8 @@ class Chart(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=255, default='DRAFT', choices=CHART_STATUS_CHOICES)
-
+    geotable = models.CharField(max_length=255, default='pollster_zip_codes', choices=settings.GEOMETRY_TABLES)
+    
     class Meta:
         ordering = ['survey', 'shortname']
         unique_together = ('survey', 'shortname')
@@ -1055,18 +1056,20 @@ class Chart(models.Model):
 
     def update_table(self):
         table_query = self.sqlsource
+        geo_table = self.geotable
         if table_query:
             table = self.get_table_name()
             view = self.get_view_name()
+            
             if re.search(r'\bzip_code_country\b', table_query):
                 view_query = """SELECT A.*, B.id AS OGC_FID, B.geometry
-                                  FROM pollster_zip_codes B, (SELECT * FROM %s) A
+                                  FROM %s B, (SELECT * FROM %s) A
                                  WHERE upper(A.zip_code_key) = upper(B.zip_code_key)
-                                   AND upper(A.zip_code_country) = upper(B.country)""" % (table,)
+                                   AND upper(A.zip_code_country) = upper(B.country)""" % (geo_table, table,)
             else:
                 view_query = """SELECT A.*, B.id AS OGC_FID, B.geometry
-                                  FROM pollster_zip_codes B, (SELECT * FROM %s) A
-                                 WHERE upper(A.zip_code_key) = upper(B.zip_code_key)""" % (table,)
+                                  FROM %s B, (SELECT * FROM %s) A
+                                 WHERE upper(A.zip_code_key) = upper(B.zip_code_key)""" % (geo_table, table,)
             cursor = connection.cursor()
             #try:
             cursor.execute("DROP VIEW IF EXISTS %s" % (view,))
@@ -1146,15 +1149,16 @@ class Chart(models.Model):
             return (None, [])
 
     def load_zip_coords(self, zip_code_key, zip_code_country=None):
+        geo_table = self.geotable
         if zip_code_country:
             query = """SELECT ST_Y(ST_Centroid(geometry)) AS lat, ST_X(ST_Centroid(geometry)) AS lng
-                         FROM pollster_zip_codes WHERE zip_code_key = %s AND country = %s"""
-            args = (zip_code_key, zip_code_country)
+                         FROM %s WHERE zip_code_key = %s AND country = %s"""
+            args = (geo_table,zip_code_key, zip_code_country)
 
         else:
             query = """SELECT ST_Y(ST_Centroid(geometry)) AS lat, ST_X(ST_Centroid(geometry)) AS lng
-                         FROM pollster_zip_codes WHERE zip_code_key = %s"""
-            args = (zip_code_key,)
+                         FROM %s WHERE zip_code_key = %s"""
+            args = (geo_table,zip_code_key,)
         try:
             cursor = connection.cursor()
             cursor.execute(query, args)

apps/pollster/migrations/0013_add_geometry_table_field.py
@@ -1,0 +1,202 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Chart.geotable'
+        db.add_column('pollster_chart', 'geotable', self.gf('django.db.models.fields.CharField')(default='pollster_zip_codes', max_length=255), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Chart.geotable'
+        db.delete_column('pollster_chart', 'geotable')
+
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'pollster.chart': {
+            'Meta': {'ordering': "['survey', 'shortname']", 'unique_together': "(('survey', 'shortname'),)", 'object_name': 'Chart'},
+            'chartwrapper': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'geotable': ('django.db.models.fields.CharField', [], {'default': "'pollster_zip_codes'", 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'shortname': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'db_index': 'True'}),
+            'sqlfilter': ('django.db.models.fields.CharField', [], {'default': "'NONE'", 'max_length': '255'}),
+            'sqlsource': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'DRAFT'", 'max_length': '255'}),
+            'survey': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Survey']"}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.ChartType']"}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'pollster.charttype': {
+            'Meta': {'object_name': 'ChartType'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'shortname': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'})
+        },
+        'pollster.option': {
+            'Meta': {'ordering': "['question', 'ordinal']", 'object_name': 'Option'},
+            'clone': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Option']", 'null': 'True', 'blank': 'True'}),
+            'column': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.QuestionColumn']", 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'group': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_open': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_virtual': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ordinal': ('django.db.models.fields.IntegerField', [], {}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Question']"}),
+            'row': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.QuestionRow']", 'null': 'True', 'blank': 'True'}),
+            'starts_hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'text': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '4095', 'blank': 'True'}),
+            'value': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'virtual_inf': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'virtual_regex': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'virtual_sup': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'virtual_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.VirtualOptionType']", 'null': 'True', 'blank': 'True'})
+        },
+        'pollster.question': {
+            'Meta': {'ordering': "['survey', 'ordinal']", 'object_name': 'Question'},
+            'data_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'data_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.QuestionDataType']"}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'error_message': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_mandatory': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'open_option_data_type': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'questions_with_open_option'", 'null': 'True', 'to': "orm['pollster.QuestionDataType']"}),
+            'ordinal': ('django.db.models.fields.IntegerField', [], {}),
+            'regex': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1023', 'blank': 'True'}),
+            'starts_hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'survey': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Survey']"}),
+            'tags': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visual': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        'pollster.questioncolumn': {
+            'Meta': {'ordering': "['question', 'ordinal']", 'object_name': 'QuestionColumn'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ordinal': ('django.db.models.fields.IntegerField', [], {}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'column_set'", 'to': "orm['pollster.Question']"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        'pollster.questiondatatype': {
+            'Meta': {'object_name': 'QuestionDataType'},
+            'css_class': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'db_type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'js_class': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        'pollster.questionrow': {
+            'Meta': {'ordering': "['question', 'ordinal']", 'object_name': 'QuestionRow'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ordinal': ('django.db.models.fields.IntegerField', [], {}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'row_set'", 'to': "orm['pollster.Question']"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        'pollster.rule': {
+            'Meta': {'object_name': 'Rule'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_sufficient': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'object_options': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'object_of_rules'", 'symmetrical': 'False', 'to': "orm['pollster.Option']"}),
+            'object_question': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'object_of_rules'", 'null': 'True', 'to': "orm['pollster.Question']"}),
+            'rule_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.RuleType']"}),
+            'subject_options': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'subject_of_rules'", 'symmetrical': 'False', 'to': "orm['pollster.Option']"}),
+            'subject_question': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'subject_of_rules'", 'to': "orm['pollster.Question']"})
+        },
+        'pollster.ruletype': {
+            'Meta': {'object_name': 'RuleType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'js_class': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        'pollster.survey': {
+            'Meta': {'object_name': 'Survey'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Survey']", 'null': 'True', 'blank': 'True'}),
+            'shortname': ('django.db.models.fields.SlugField', [], {'default': "''", 'max_length': '255', 'db_index': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'DRAFT'", 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'version': ('django.db.models.fields.SlugField', [], {'default': "''", 'max_length': '255', 'db_index': 'True', 'blank': 'True'})
+        },
+        'pollster.surveychartplugin': {
+            'Meta': {'object_name': 'SurveyChartPlugin', 'db_table': "'cmsplugin_surveychartplugin'", '_ormbases': ['cms.CMSPlugin']},
+            'chart': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Chart']"}),
+            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'pollster.translationoption': {
+            'Meta': {'ordering': "['translation', 'option']", 'unique_together': "(('translation', 'option'),)", 'object_name': 'TranslationOption'},
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'option': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Option']"}),
+            'text': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '4095', 'blank': 'True'}),
+            'translation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.TranslationSurvey']"})
+        },
+        'pollster.translationquestion': {
+            'Meta': {'ordering': "['translation', 'question']", 'unique_together': "(('translation', 'question'),)", 'object_name': 'TranslationQuestion'},
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'error_message': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Question']"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'translation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.TranslationSurvey']"})
+        },
+        'pollster.translationquestioncolumn': {
+            'Meta': {'ordering': "['translation', 'column']", 'unique_together': "(('translation', 'column'),)", 'object_name': 'TranslationQuestionColumn'},
+            'column': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.QuestionColumn']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'translation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.TranslationSurvey']"})
+        },
+        'pollster.translationquestionrow': {
+            'Meta': {'ordering': "['translation', 'row']", 'unique_together': "(('translation', 'row'),)", 'object_name': 'TranslationQuestionRow'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'row': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.QuestionRow']"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'translation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.TranslationSurvey']"})
+        },
+        'pollster.translationsurvey': {
+            'Meta': {'ordering': "['survey', 'language']", 'unique_together': "(('survey', 'language'),)", 'object_name': 'TranslationSurvey'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '3', 'db_index': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'DRAFT'", 'max_length': '255'}),
+            'survey': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.Survey']"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        },
+        'pollster.virtualoptiontype': {
+            'Meta': {'object_name': 'VirtualOptionType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'js_class': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'question_data_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pollster.QuestionDataType']"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['pollster']

settings.py
@@ -123,6 +123,10 @@ CMS_TEMPLATES = (
     ('base/influhome.html', "European Map"),
 )
 
+GEOMETRY_TABLES = (
+ ('pollster_zip_codes','zip level'),
+)
+
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (

commit b16c46ae084301d60d2988711ecc978b58c6d7ee

Fixed bug introduced in 59d11efee8a014eb018; table-names are not values and must not be escaped

apps/pollster/models.py
@@ -907,6 +907,7 @@ class Chart(models.Model):
                 shortname = settings.POLLSTER_USER_PROFILE_SURVEY
                 survey = Survey.objects.get(shortname=shortname, status='PUBLISHED')
                 lpd = survey.get_last_participation_data(user_id, global_id)
+                print 'about to not happen',lpd ,hasattr(settings, 'POLLSTER_USER_ZIP_CODE_DATA_NAME')
                 if lpd and hasattr(settings, 'POLLSTER_USER_ZIP_CODE_DATA_NAME'):
                     zip_code = lpd.get(settings.POLLSTER_USER_ZIP_CODE_DATA_NAME)
                     if zip_code is not None:
@@ -1152,13 +1153,13 @@ class Chart(models.Model):
         geo_table = self.geotable
         if zip_code_country:
             query = """SELECT ST_Y(ST_Centroid(geometry)) AS lat, ST_X(ST_Centroid(geometry)) AS lng
-                         FROM %s WHERE zip_code_key = %s AND country = %s"""
-            args = (geo_table,zip_code_key, zip_code_country)
+                         FROM """ + geo_table + """ WHERE zip_code_key = %s AND country = %s"""
+            args = (zip_code_key, zip_code_country)
 
         else:
             query = """SELECT ST_Y(ST_Centroid(geometry)) AS lat, ST_X(ST_Centroid(geometry)) AS lng
-                         FROM %s WHERE zip_code_key = %s"""
-            args = (geo_table,zip_code_key,)
+                         FROM """ + geo_table + """ WHERE zip_code_key = %s"""
+            args = (zip_code_key,)
         try:
             cursor = connection.cursor()
             cursor.execute(query, args)