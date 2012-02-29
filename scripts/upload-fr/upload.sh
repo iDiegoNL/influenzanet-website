cedb0733339d36d4a9b432cfb555e2e4645336df
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-02-29 16:11:28 +0100

    host dest dir

--- scripts/upload-fr/upload.sh
+++ scripts/upload-fr/upload.sh
@@ -13,7 +13,7 @@ echo "Creating dump"
 pg_dump -t epidb_results_intake -t epidb_results_weekly --clean --no-owner $DBNAME > epidb_results.sql
 
 echo "Uploading"
-scp -v epidb_results.sql $HOST
+scp -v epidb_results.sql $HOST:.
 
 echo "Importing data in target host"
 ssh $HOST 'psql -f epidb_results.sql'
