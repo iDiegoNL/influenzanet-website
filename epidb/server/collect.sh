fa7b6cac8dd37457928b380aefa8c3bfffc8b401
Klaas van Schelven <klaas@vanschelven.com>
2012-04-30 18:24:27 +0200

    Upload to Marketplace

--- epidb/server/collect.sh
+++ epidb/server/collect.sh
@@ -13,11 +13,19 @@ pg_dump --data-only -t epidb_results_intake -t epidb_results_weekly gripenet >> 
 #pg_dump --data-only -t epidb_results_intake -t epidb_results_weekly grippenet >> /home/epidb/data/epidb_results.sql
 
 #echo influensakoll
-pg_dump --data-only -t epidb_results_intake -t epidb_results_weekly influensakoll >> /home/epidb/data/epidb_results.sql
+# influensakoll appears to be broken
+#pg_dump --data-only -t epidb_results_intake -t epidb_results_weekly influensakoll >> /home/epidb/data/epidb_results.sql
 
 #echo influweb
 pg_dump --data-only -t epidb_results_intake -t epidb_results_weekly influweb >> /home/epidb/data/epidb_results.sql
 
 grep -v -e '^GRANT' -e '^REVOKE' -e 'OWNER TO' /home/epidb/data/epidb_results.sql > /home/epidb/data/epidb_results.sql.tmp
 mv /home/epidb/data/epidb_results.sql.tmp /home/epidb/data/epidb_results.sql
+
+echo 'DROP TABLE epidb_results_weekly CASCADE;' | psql
+echo 'DROP TABLE epidb_results_intake CASCADE;' | psql
+
+psql < /home/epidb/data/epidb_results.sql
+
+cat /home/epidb/scripts/pollster_incidence.sql | psql
 
