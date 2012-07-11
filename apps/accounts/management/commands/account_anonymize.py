b168173aaa031d969de9d1b2bf4a5a5c3b62cf5a
Clément Turbelin <Clment Turbelin@CTPORTABLE2012>
2012-07-11 21:56:10 +0200

    command account anonymize

--- apps/accounts/management/commands/account_anonymize.py
+++ apps/accounts/management/commands/account_anonymize.py
@@ -3,6 +3,11 @@ from optparse import make_option
 from django.contrib.auth.models import User,UNUSABLE_PASSWORD
 from apps.survey.models import SurveyUser
 from django.db import transaction
+from random import choice
+
+# create a random string
+def random_string(length, allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'):
+    return ''.join([choice(allowed_chars) for i in range(length)])
 
 class Command(BaseCommand):
     help = 'Resend activation mail to user(s).'
@@ -25,22 +30,25 @@ class Command(BaseCommand):
             susers = SurveyUser.objects.filter(user=user)
             
             for su in susers:
-                print "anonymizing participant %d" % su.id
-                su.name = 'participant'+su.id
+                print " > anonymizing participant %d" % su.id
+                su.name = 'part' + str(su.id) +'_'+random_string(4) # random tail to expect uniqueness
                 su.save() 
 
-            name = user+'id'
+            # create a unique user name (not used)
+            name = 'user'+str(id)+'-'+random_string(6)
+            user.username = name
             user.email = name+'@'+'localhost'
             user.first_name = name
             user.last_name = name
             user.password = UNUSABLE_PASSWORD
             user.is_active = False
             user.save()
-            print "account %d anonymized" % id
+            print " > account %d anonymized" % id
             transaction.commit()
-        except:
+        except Exception as e:
             print 'error during anonymization'
             transaction.rollback()
+            print e
         
     
     def handle(self, *args, **options):
@@ -59,23 +67,26 @@ class Command(BaseCommand):
             return
 
         if user_id is not None:
-            user = User.objects.get(id=user_id)
+            u = user_id.split(',')
+            users = User.objects.filter(id__in=u)
         else:
-            user = User.objects.get(email=mail)
+            users = User.objects.filter(email=mail)
 
-        if len(user) == 0:
+        if len(users) == 0:
             print 'User not found'
             return
-        
-        print "User found: %d" % user.id
-        print "Name: %s %s " % (user.first_name, user.last_name)
-        print "login: %s " % (user.username, user.last_name)
-        print "email: %s " % (user.email)
-        
-        confirm = raw_input("Confirm (yes/no")
-        if confirm == "yes":
-            if fake:
-                print "fake anonymizing user " % user.id
+        print "%d user(s) found" % len(users)
+        print '----'
+        for user in users:
+            print "- User found: %d" % user.id
+            print "  Name: %s %s " % (user.first_name, user.last_name)
+            print "  login: %s " % (user.username)
+            print "  email: %s " % (user.email)
+            confirm = raw_input("Confirm (yes/no) ")
+            if confirm == "yes":
+                if fake:
+                    print "fake anonymizing user %d" % user.id
+                else:
+                    self.anonymize(user)
             else:
-                self.anonymize(user)
-        
+                print "> Cancelled for user %d" % user.id
