f47e7d1f024a3162716b3cbf8b1759475b881ce0
Klaas van Schelven <klaas@vanschelven.com>
2012-10-25 10:31:36 +0200

    Contest: changed 'no longer to fill in' date

--- apps/contest/cms_plugins.py
+++ apps/contest/cms_plugins.py
@@ -21,7 +21,7 @@ class CMSContestPlugin(CMSPluginBase):
         else:
             filename = "prijs_grafiek_weergave2012"
 
-        if date.today() > date(2012, 1, 25):
+        if date.today() > date(2012, 11, 15):
             filename = "prijs_grafiek_weergave2012" # no longer possible to enter results
 
         context.update({
