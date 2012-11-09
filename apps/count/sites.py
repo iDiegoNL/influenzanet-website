b4bdd0999b3c95061e71ff3faccbe75455aed2f3
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-11-09 13:14:13 +0100

    dont duplicate SOURCES var

--- apps/count/sites.py
+++ apps/count/sites.py
@@ -1,17 +1,6 @@
 import urllib2
 
-SOURCES = {
-    'nl': 'https://www.degrotegriepmeting.nl/count/counter?country=NL',
-    'be': 'https://www.degrotegriepmeting.nl/count/counter/?country=BE',
-    'de': 'http://www.aktivgegengrippe.de/count/counter/',
-    'at': 'http://www.aktivgegengrippe.at/count/counter/',
-    'ch': 'http://www.aktivgegengrippe.ch/count/counter/',
-    'se': 'http://www.influensakoll.se/count/counter/',
-    'uk': 'http://flusurvey.org.uk/count/counter/',
-    'it': 'http://www.influweb.it/count/counter/',
-    'pt': 'http://www.gripenet.pt/count/counter/',
-    'fr': 'https://www.grippenet.fr/count/counter/',
-}
+from apps.count.templatetags.count import SOURCES
 
 def site_fetch_count(country):
     try:
