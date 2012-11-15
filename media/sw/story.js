797eb936481aea6d7546938c4af7023e25d915c7
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-11-15 16:03:12 +0100

    update story table

--- media/sw/story.js
+++ media/sw/story.js
@@ -2,12 +2,15 @@
  * Build Household Story
  */
 
-var Story = {
-	
-	/*
-	 * Media files path 
-	 */
-	media: '',
+function StoryTable(history, users) {
+	this.history = history;
+	this.users = users;
+
+}
+ 
+StoryTable.media = ''; 
+ 
+$.extend(StoryTable.prototype, {
 	
 	/*
 	 * Build a row, with d and row values for each users
@@ -16,9 +19,9 @@ var Story = {
 		var o = '<tr>';
 		var date = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
 		o += '<td class="date">'+date+'</td>';
-		var n = users.length;
+		var n = this.users.length;
 		for(var i=0; i < n; ++i) {
-			u = users[i];
+			u = this.users[i];
 			var diag = row[u];
 			if(typeof(diag) == "undefined") {
 				diag = "&nbsp;"
@@ -37,7 +40,7 @@ var Story = {
 		if(typeof(s)== 'undefined') {
 			s = 'UNKNOWN';
 		}
-		return '<img src="'+Story.media+'/survey/img/diag-'+s+'-small.png" title="'+ h.diag +'" alt="" />';
+		return '<img src="'+StoryTable.media+'/survey/img/diag-'+s+'-small.png" title="'+ h.diag +'" alt="" />';
 	},
 	
 	ymd: function(d) {
@@ -49,31 +52,31 @@ var Story = {
 		var row = {};
 		var out = '<table>';
 		
-		var n = users.length;
+		var n = this.users.length;
 		out +='<tr>';
 		out += '<th>Date</th>';
 		for(var i=0; i < n; ++i) {
-			out += '<th>' + users[i] + '</th>';
+			out += '<th>' + this.users[i] + '</th>';
 		}
 		out += '</tr>';
-		n = history.length;
+		n = this.history.length;
 		for(var i=0; i < n; ++i) {
-			var h = history[i];
+			var h = this.history[i];
 			if(i == 0)  {
 				d = h.date;
 			}
-			if( Story.ymd(d) != Story.ymd(h.date) ) {
-				out += Story.buildRow(d, row);
+			if( this.ymd(d) != this.ymd(h.date) ) {
+				out += this.buildRow(d, row);
 				row = {};
 				d = h.date;	
 			}
-			row[ h.user ] = Story.buildDiag(h);
+			row[ h.user ] = this.buildDiag(h);
 		}
-		out += Story.buildRow(d, row);
+		out += this.buildRow(d, row);
 		out += '</table>';
-		$(container).html(out);
+		$('#'+container).html(out);
 	}
-};
+});
 
 
 
