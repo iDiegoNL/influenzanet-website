80b3ea116ed0c14516819cf3c1facee1cafae992
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-11-15 11:03:55 +0100

    lastyear query

--- apps/pollster/lastyear.py
+++ apps/pollster/lastyear.py
@@ -2,6 +2,6 @@ from django.db import connection
 
 def get_previous_year_data(user_id, global_id, shortname):
  cursor = connection.cursor()
- query = "select * from pollster_%s_lastyear where global_id='%s' and user='%s'" % (shortname, global_id, str(user_id))
+ query = "select * from pollster_%s_lastyear where \"global_id\"='%s' and \"user\"='%s'" % (shortname, global_id, str(user_id))
  cursor.execute(query)
  return cursor.fetchone()
