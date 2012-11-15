4c400c9530ca6b76aa6a8ef5dd871e5a92cf705e
Clément Turbelin <Clment Turbelin@CTFixe109>
2012-11-15 11:15:29 +0100

    get colnames

--- apps/pollster/lastyear.py
+++ apps/pollster/lastyear.py
@@ -4,4 +4,6 @@ def get_previous_year_data(user_id, global_id, shortname):
  cursor = connection.cursor()
  query = "select * from pollster_%s_lastyear where \"global_id\"='%s' and \"user\"='%s'" % (shortname, global_id, str(user_id))
  cursor.execute(query)
- return cursor.fetchone()
+ desc = cursor.description
+ res = cursor.fetchone()
+ return dict(zip([col[0] for col in desc], res))
