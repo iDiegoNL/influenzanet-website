ab139d2f8429cbfdd35387a0daa1baedfefc3030
Clément Turbelin <Clment Turbelin@CTPORTABLE2012>
2013-01-09 12:03:21 +0100

    update translations

--- apps/sw_auth/templatetags/user_login.py
+++ apps/sw_auth/templatetags/user_login.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 
 from django.template import Library
-from django.core.cache import cache 
-from apps.sw_auth.models import EpiworkUser, FakedUser
+from apps.sw_auth.models import FakedUser
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
 register = Library()
 
 
@@ -11,6 +12,6 @@ def user_login(user):
     if isinstance(user, FakedUser):
         return user.username
     elif isinstance(user, User):
-        return 'Anonymous'
+        return _('Anonymous')
 
 register.filter('user_login', user_login)
