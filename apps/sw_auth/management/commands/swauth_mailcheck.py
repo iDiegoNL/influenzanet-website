cd65751c0991f0b5ee26159881e1189f9c6844c4
Clément Turbelin <Clment Turbelin@CTPORTABLE2012>
2013-02-12 10:15:41 +0100

    mailcheck to json

--- apps/sw_auth/management/commands/swauth_mailcheck.py
+++ apps/sw_auth/management/commands/swauth_mailcheck.py
@@ -1,21 +1,41 @@
 from django.core.management.base import CommandError, BaseCommand
 from ...models import EpiworkUser
 from django.conf import settings
+from optparse import make_option
+from django.utils import simplejson
 
 class Command(BaseCommand):
     help = 'Build domains list'
 
+    option_list = BaseCommand.option_list + (
+        make_option('-m', '--min', action='store', dest='min', default=10, help='Minimum number of mail to include the domain'),
+    )
+
     def handle(self, *args, **options):
         domains = {}
+        threshold = int(options['min'])
+        verbose = options['verbosity'] > 0
         for user in EpiworkUser.objects.all():
             email = user.email
             u, d = email.split('@')
             if domains.has_key(d):
-                ++domains[d]
+                domains[d] = domains[d] + 1
             else:
                 domains[d] = 1
-        fn = settings.MEDIA_ROOT + '/sw/js/maildomains.js';
-        f = file(fn,'w')
-        s = " my_domains = ['"+ "','".join(domains.keys()) + "'];"
-        f.write(s)
-        f.close()
+                
+        dd = []
+        for (d,v) in domains.items():
+            if verbose:
+                print d + ":" + str(v)
+            if int(v) >= threshold:
+                dd.append(d)  
+        domains = dd
+        if len(domains) > 0:
+            fn = settings.MEDIA_ROOT + '/assets/domains.json';
+            f = file(fn,'w')
+            s = simplejson.dumps(domains)
+            f.write(s)
+            f.close()
+            print "%s updated" % fn
+        else:
+            print "No domain to include"
