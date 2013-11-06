eb1c3fc02b2a8bca02c9741f6a984659fa214341
Clément Turbelin <clement.turbelin@gmail.com>
2013-11-07 00:15:25 +0100

    user menu, remove fake avatar

--- apps/sw_auth/templatetags/user_login.py
+++ apps/sw_auth/templatetags/user_login.py
@@ -4,13 +4,14 @@ from django.template import Library
 from apps.sw_auth.models import FakedUser
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-
+from django.utils.safestring import mark_safe
 register = Library()
 
 
 def user_login(user):
     if isinstance(user, FakedUser):
-        return user.username
+        s = """<span class="user-login">%s</span><br/><span class="user-email">%s</span>""" % (user.username, user.email,)
+        return mark_safe(s)
     elif isinstance(user, User):
         return _('Anonymous')
 
