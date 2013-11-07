29ceae21179dd6659173c2f32cce136188747ffa
Clément Turbelin <clement.turbelin@gmail.com>
2013-11-07 18:55:28 +0100

    connected user layout

--- apps/sw_auth/templatetags/user_login.py
+++ apps/sw_auth/templatetags/user_login.py
@@ -2,17 +2,10 @@ from __future__ import absolute_import
 
 from django.template import Library
 from apps.sw_auth.models import FakedUser
-from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
-from django.utils.safestring import mark_safe
 register = Library()
 
 
 def user_login(user):
-    if isinstance(user, FakedUser):
-        s = """<span class="user-login">%s</span><br/><span class="user-email">%s</span>""" % (user.username, user.email,)
-        return mark_safe(s)
-    elif isinstance(user, User):
-        return _('Anonymous')
+    return isinstance(user, FakedUser)
 
 register.filter('user_login', user_login)
