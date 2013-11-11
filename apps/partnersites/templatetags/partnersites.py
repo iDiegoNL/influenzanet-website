e060a8954752c20ad7637f80c5de9f7185619e9a
Federico Di Gregorio <fog@initd.org>
2013-11-11 16:53:03 +0000

    Changed urchin URL

--- apps/partnersites/templatetags/partnersites.py
+++ apps/partnersites/templatetags/partnersites.py
@@ -23,6 +23,8 @@ class GoogleAnalyticsNode(Node):
 
         return ("""<script type="text/plain" class="cc-onconsent-analytics">""" if site_settings.show_cookie_warning else """<script type="text/javascript">""") + \
 """  var _gaq = _gaq || [];
+  var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+  _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
   _gaq.push(['_setAccount', '%s']);
   _gaq.push(['_setDomainName', 'none']);
   _gaq.push(['_setAllowLinker', true]);
@@ -30,10 +32,11 @@ class GoogleAnalyticsNode(Node):
 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
 </script>""" % settings.GOOGLE_ANALYTICS_ACCOUNT
 
 register.tag('google_analytics', do_google_analytics)
+
