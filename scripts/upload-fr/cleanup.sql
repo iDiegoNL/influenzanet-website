550e048444f2594e56d1ee6a57d8986db029a067
cturbelin <clement.turbelin@gmail.com>
2014-03-05 14:03:06 +0100

    upload script, cleanup target db

---
+++ scripts/upload-fr/cleanup.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS pollster_results_weekly;
+DROP TABLE IF EXISTS pollster_results_intake;
+DROP TABLE IF EXISTS epidb_results_intake;
+DROP TABLE IF EXISTS epidb_results_weekly;
--- scripts/upload-fr/upload.sh
+++ scripts/upload-fr/upload.sh
@@ -2,24 +2,28 @@
 set -e
 
 HOST=epidb@www.influweb.it:upload/
-DBNAME=epiwork
+SOURCE_DBNAME=epiwork
+TARGET_DBNAME=epipop
 COUNTRY='fr'
 
 DIR=`dirname $0`
 
 echo "Creating export tables"
-psql -f $DIR/dump.sql $DBNAME
+psql -f $DIR/dump.sql $SOURCE_DBNAME
 
 echo "Creating dump"
-pg_dump -Fc -x -O -t epidb_results_intake -t epidb_results_weekly --clean --no-owner $DBNAME > epidb_results_fr.dump
+pg_dump -Fc -x -O -t epidb_results_intake -t epidb_results_weekly --clean --no-owner $SOURCE_DBNAME > epidb_results_fr.dump
 
-echo "Installing dump"
-pg_restore -d epipop --no-owner --clean --create < epidb_results_fr.dump
+echo "Cleanup target database"
+psql -f $DIR/cleanup.sql $TARGET_DBNAME
 
-psql -e -f $DIR/rename.sql epipop
+echo "Installing dump in target db"
+pg_restore -d $TARGET_DBNAME --no-owner --create < epidb_results_fr.dump
 
-echo "Creating final dump"
-pg_dump -Fc -x -O -t pollster_results_intake -t pollster_results_weekly epipop > pollster_results_$COUNTRY.dump
+psql -e -f $DIR/rename.sql $TARGET_DBNAME
+
+echo "Creating final dump from target db"
+pg_dump -Fc -x -O -t pollster_results_intake -t pollster_results_weekly $TARGET_DBNAME > pollster_results_$COUNTRY.dump
 
 echo "Uploading"
 scp -Cq pollster_results_$COUNTRY.dump $HOST
