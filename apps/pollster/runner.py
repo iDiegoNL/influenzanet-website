62d861e5f9b5d889b2df804c4ebd6c19c6d4503d
Clément Turbelin <clement.turbelin@gmail.com>
2014-11-02 19:14:21 +0100

    next url handling

--- apps/pollster/runner.py
+++ apps/pollster/runner.py
@@ -37,14 +37,6 @@ def update_url_params(url, params):
     url = urlparse.urlunparse(url_parts)
     return url
 
-def get_next_url(url, global_id):
-    next_url_parts = list(urlparse.urlparse(url))
-    query = dict(urlparse.parse_qsl(next_url_parts[4]))
-    query.update({'gid': global_id})
-    next_url_parts[4] = urllib.urlencode(query)
-    next_url = urlparse.urlunparse(next_url_parts)
-    return next_url
-
 class BaseWorkflow(object):
     
     def user_has_data(self, shortname, survey_user):
@@ -98,16 +90,12 @@ class SurveyContext(object):
         self.language = None 
         self.last_data = None
         self.template = 'pollster/survey_run.html'
-        self.data = {}
-    
-    def add_template_data(self, name, value):
-        self.data[name] = value
-        
+            
     def get_template_data(self):
         """
          get the data ready to be transmitted to the survey template
         """
-        self.data.update({
+        return {
           "language": self.language,
           "locale_code": get_locale(self.language),
           "survey": self.survey,
@@ -115,8 +103,7 @@ class SurveyContext(object):
           "last_participation_data_json": as_json(self.last_data),
           "form": self.form,
           "person": self.survey_user,
-        })
-        return self.data
+        }
 
 class SurveyRunner(object):
     """
@@ -217,11 +204,11 @@ class SurveyRunner(object):
                     if response and isinstance(response, HttpResponse):
                         return response
                     if response and isinstance(response, basestring):
-                        next = response
+                        next_url = response
                 
-                if next:
-                    next = self.get_next_url(next, global_id)
-                return HttpResponseRedirect(next)
+                if next_url:
+                    next_url = update_url_params(next_url, {"gid": global_id})
+                return HttpResponseRedirect(next_url)
             else:
                 survey.set_form(form)
 
--- apps/survey/workflow/influenzanet.py
+++ apps/survey/workflow/influenzanet.py
@@ -3,6 +3,9 @@
 ##
 from django.conf import settings 
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
 
 from . import SurveyWorkflow
 
@@ -21,7 +24,22 @@ class InfluenzanetWorkflow(SurveyWorkflow):
         if shortname != PROFILE_SURVEY:
             su = context.survey_user
             if not self.user_has_data(PROFILE_SURVEY, su):
+                m = _(u'Before we take you to the symptoms questionnaire, please complete the short background questionnaire below. You will only have to complete this once.')
+                messages.add_message(context.request, messages.INFO, m )
                 return HttpResponseRedirect(self.get_survey_url(PROFILE_SURVEY, su, next_survey=shortname))
         
-            
+    def after_save(self, context):
+        next_url = None
+        if 'next' not in context.request.GET:
+            shortname = context.survey.shortname
+            if shortname == 'intake':
+                next_url = reverse('profile_thanks')
+            elif shortname == "weekly":
+                next_url = "/dashboard"
+            else:
+                next_url = reverse('survey_run_thanks', {"shortname": shortname})
+        else:
+            next_url = context.request.GET
+        return next_url
+        
     
