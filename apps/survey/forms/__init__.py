a906679ebf6961053fe26c02379abfd202406539
Clément Turbelin <clement.turbelin@gmail.com>
2014-11-09 21:08:05 +0100

    restore addPeople old form version

--- apps/survey/forms/__init__.py
+++ apps/survey/forms/__init__.py
@@ -3,4 +3,6 @@ from django.utils.translation import ugettext as _
 
 class AddPeople(forms.Form):
     name = forms.CharField(label=_('Name'), max_length=100)
+    avatar = forms.IntegerField(widget=forms.HiddenInput(), required=False, initial=0)
+
 
