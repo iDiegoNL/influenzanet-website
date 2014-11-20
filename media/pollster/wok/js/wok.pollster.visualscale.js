74eb7fc332098ad8d9f695c264fd617dfad9cb97
cturbelin <clement.turbelin@gmail.com>
2014-11-20 15:15:32 +0100

    change visual scale min label

--- media/pollster/wok/js/wok.pollster.visualscale.js
+++ media/pollster/wok/js/wok.pollster.visualscale.js
@@ -26,7 +26,7 @@
 								$('#' + id).val(ui.value);
 							}
 						});
-						$field.after('<div class="slider-range"><div id="slider-min" ><span class="vas-worst-level"></span><br/>Très mauvais</div><div id="slider-max"><span class="vas-best-level"></span><br/>Tout va bien</div><div style="clear:both">&nbsp;</div><div class="vas-help">Glissez le curseur pour le placer sur la barre jusqu\'au niveau qui correspond le mieux à l\'état de santé global du participant aujourd\'hui</div></div>');
+						$field.after('<div class="slider-range"><div id="slider-min" ><span class="vas-worst-level"></span><br/>Très mal</div><div id="slider-max"><span class="vas-best-level"></span><br/>Tout va bien</div><div style="clear:both">&nbsp;</div><div class="vas-help">Glissez le curseur pour le placer sur la barre jusqu\'au niveau qui correspond le mieux à l\'état de santé global du participant aujourd\'hui</div></div>');
 						$wrap = $('<div class="slider-wrap"</div>');
 						$wrap.append($slider);
 						$field.after($wrap);
