80af04af524db53be6c6d8ef799e8d159116ce46
Federico Di Gregorio <fog@initd.org>
2015-01-14 08:45:02 +0000

    Updated dashboard queries to 2014/2015 season

--- sql/postgresql/pollster_dashboard.sql
+++ sql/postgresql/pollster_dashboard.sql
@@ -107,8 +107,8 @@ AS  $$
                A.zip_code_key, 
                I.timestamp 
         FROM (SELECT X.country, X.zip_code_key 
-                FROM pollster_zip_codes X
-               WHERE ST_Touches(geometry, (SELECT geometry FROM pollster_zip_codes Y WHERE Y.zip_code_key = $1))
+                FROM pollster_zip_codes X, (SELECT geometry FROM pollster_zip_codes Y WHERE Y.zip_code_key = $1) G
+               WHERE ST_Touches(X.geometry, G.geometry)
               ) A 
 	INNER JOIN pollster_dashboard_last_intake I ON (A.zip_code_key = I."Q3"::text);
         END
@@ -217,7 +217,8 @@ CREATE OR REPLACE FUNCTION pollster_dashboard_badges_by_global_id(
 ) RETURNS TABLE (
   global_id     varchar(36),
   "user"       	integer,
-  is_loyal 	boolean,
+  is_loyal_2012_2013 	boolean,
+  is_loyal_2013_2014	boolean,
   is_novice	boolean,
   is_junior	boolean,
   is_senior	boolean,
@@ -231,32 +232,39 @@ AS
 $$
 BEGIN
 RETURN QUERY
-SELECT DISTINCT 
-			 A.global_id,
-			 user_id AS "user",
-			 sum(loyal_candidate) OVER (PARTITION BY A.global_id) > 0 
-			        AND (SELECT count(*) 
-			        FROM pollster_results_intake AS I 
-			       WHERE (timestamp <= '2013-04-30' AND I.global_id = A.global_id)) > 0
-                         AS is_loyal,
-			 count(*) OVER (PARTITION BY A.global_id) >= 1 AS is_novice,
-			 count(*) OVER (PARTITION BY A.global_id) >= 3 AS is_junior,
-			 count(*) OVER (PARTITION BY A.global_id) >= 6 AS is_senior,
-			 count(*) OVER (PARTITION BY A.global_id) >= 10 AS is_gold,
-			 count(*) OVER (PARTITION BY A.global_id) >= 20 AS is_platinum,
-			 sum(cold) OVER (PARTITION BY A.global_id) >= 3 AS is_cold_season,
-			 sum(ili) OVER (PARTITION BY A.global_id)  >= 2 AS is_ili_season,
-			 sum(gastro) OVER (PARTITION BY A.global_id) >= 2 AS is_gastro_season
-	FROM (
-
+SELECT DISTINCT
+                         A.global_id,
+                         user_id AS "user",
+                         sum(loyal_candidate_2012_2013) OVER (PARTITION BY A.global_id) > 0
+                                AND (SELECT count(*)
+                                FROM pollster_results_intake AS I
+                               WHERE (timestamp <= '2013-04-30' AND I.global_id = A.global_id)) > 0
+                         AS is_loyal_2012_2013,
+                         sum(loyal_candidate_2013_2014) OVER (PARTITION BY A.global_id) > 0
+                                AND (SELECT count(*)
+                                FROM pollster_results_intake AS I
+                               WHERE (timestamp > '2013-04-30' AND timestamp <= '2014-04-30' AND I.global_id = A.global_id)) > 0
+                         AS is_loyal_2013_2014,
+                         count(is_current_season) OVER (PARTITION BY A.global_id) >= 1 AS is_novice,
+                         count(is_current_season) OVER (PARTITION BY A.global_id) >= 3 AS is_junior,
+                         count(is_current_season) OVER (PARTITION BY A.global_id) >= 6 AS is_senior,
+                         count(is_current_season) OVER (PARTITION BY A.global_id) >= 10 AS is_gold,
+                         count(is_current_season) OVER (PARTITION BY A.global_id) >= 20 AS is_platinum,
+                         sum(cold) OVER (PARTITION BY A.global_id) >= 3 AS is_cold_season,
+                         sum(ili) OVER (PARTITION BY A.global_id)  >= 2 AS is_ili_season,
+                         sum(gastro) OVER (PARTITION BY A.global_id) >= 2 AS is_gastro_season
+        FROM (
 SELECT DISTINCT HS.global_id,
-			 user_id,			 
-		   to_char(timestamp, 'YYYYWW'),
-		   CASE true WHEN (timestamp >= '2013-04-30') THEN 1 ELSE 0 END as loyal_candidate,		   
-		   CASE true WHEN (status ILIKE '%COLD%') THEN 1 ELSE 0 END as cold,
-		   CASE true WHEN (status = 'ILI') THEN 1 ELSE 0 END as ili,
-		   CASE true WHEN (status ILIKE '%GASTRO%') THEN 1 ELSE 0 END as gastro
+                         user_id,
+                   to_char(timestamp, 'YYYYWW') AS ts,
+                   CASE WHEN timestamp > '2014-9-1' THEN true ELSE null END AS is_current_season,
+                   CASE true WHEN (timestamp > '2012-04-30' AND timestamp <= '2013-04-30') THEN 1 ELSE 0 END as loyal_candidate_2012_2013,
+                   CASE true WHEN (timestamp > '2013-04-30' AND timestamp <= '2014-04-30') THEN 1 ELSE 0 END as loyal_candidate_2013_2014,                   
+                   CASE true WHEN (status ILIKE '%COLD%' AND timestamp > '2014-04-30') THEN 1 ELSE 0 END as cold,
+                   CASE true WHEN (status = 'ILI' AND timestamp > '2014-04-30') THEN 1 ELSE 0 END as ili,
+                   CASE true WHEN (status ILIKE '%GASTRO%' AND timestamp > '2014-04-30') THEN 1 ELSE 0 END as gastro
   FROM pollster_dashboard_healt_status HS WHERE HS.global_id = $1) A;
+
 END
 $$
 LANGUAGE 'plpgsql';
