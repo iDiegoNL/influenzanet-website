3be273034426773110853853a14fd28a75a1098a
cturbelin <clement.turbelin@gmail.com>
2015-11-24 11:50:01 +0100

    fix auto_now to auto_now_add

--- apps/grippenet/models.py
+++ apps/grippenet/models.py
@@ -4,25 +4,25 @@ from apps.survey.models import SurveyUser
 
 class PregnantCohort(models.Model):
     survey_user = models.ForeignKey(SurveyUser)
-    
+
     # Inclusion date
-    date_created = models.DateField(auto_now=True)
-    
+    date_created = models.DateField(auto_now_add=True)
+
     # Is active in cohort
     active = models.BooleanField(default=True)
-    
+
     # Do need to change the channel on the survey data
     change_channel = models.BooleanField(default=True)
-    
+
     # Date when reminder should be sended from
     date_reminder = models.DateField(null=True)
-    
+
     # Count of reminder sent
     reminder_count = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return '<Pregnant' + str(self.survey_user.id) +'>'
-    
+
 class Participation(models.Model):
     survey_user = models.ForeignKey(SurveyUser)
     first_season = models.IntegerField()
