53104510b4da7b55b03caf68fcebb069f54f79d3
cturbelin <clement.turbelin@gmail.com>
2016-11-30 17:00:12 +0100

    fix graph when no graph data

--- media/sw/js/graph.js
+++ media/sw/js/graph.js
@@ -34,16 +34,19 @@ function calc_user_group(profile, age_group) {
 	return {'id': profile.gender + '_' + group, 'label': label};
 }
 
- 
+
 function prepare_data(data, column, my_group, translation) {
-	var values = [], 
+	var values = [],
 	labels = [], // Bar labels
 	colors = [], // Bar colors
 	my_group_idx, // index of the group the user belongs to
-	vv, i, label, 
+	vv, i, label,
 	units;
 	i = 0;
 	console.log(data);
+	if(!data) {
+		return false;
+	}
 	my_group_idx = -1;
 	vv = data[column];
 	if(column == 'prop') {
@@ -72,23 +75,23 @@ function prepare_data(data, column, my_group, translation) {
 		labels: labels,
 		colors: colors,
 		units: units,
-		my_group: my_group_idx	
+		my_group: my_group_idx
 	};
-} 
+}
 
 function create_labels(paper, barchart, horiz, data, chart_margin, label_margin, bar_label, show_avatar) {
 	var set = paper.set(),
 		covers = barchart.covers,
 		bars = barchart.bars,
 		n = covers.length;
-		
+
 	for(var i = 0; i < n; ++i) {
 		// Left label
-		var 
+		var
 			bin = covers[i].getBBox(), // bin bounds
 			bb, base,
 			x, y;
-		
+
 		if(horiz) {
 			x = 0;
 			y = bin.y + (bin.height / 2);
@@ -101,10 +104,10 @@ function create_labels(paper, barchart, horiz, data, chart_margin, label_margin,
 			w =  label.getBBox().width;
 			if(horiz) {
 				label.attr('x', chart_margin - w - label_margin )
-			} 
+			}
 			set.push(label);
 		}
-		
+
 		// Value label on the right of the bar
 		bb = bars[i].getBBox();
 		if(horiz) {
@@ -115,7 +118,7 @@ function create_labels(paper, barchart, horiz, data, chart_margin, label_margin,
 			}
 		} else {
 			if(bb.height == 0) {
-				y = bin.height - label_margin;	
+				y = bin.height - label_margin;
 			} else {
 				y = bb.y - label_margin;
 			}
@@ -123,7 +126,7 @@ function create_labels(paper, barchart, horiz, data, chart_margin, label_margin,
 		var text = data.values[i] + data.units;
 		label = paper.text(x, y, text );
 		set.push(label);
-		
+
 		if( show_avatar && i == data.my_group ) {
 			bb = label.getBBox();
 			//label = paper.text(bin.x + bin.width/2, bin.y + bin.height/2, "Vous");
@@ -132,16 +135,16 @@ function create_labels(paper, barchart, horiz, data, chart_margin, label_margin,
 	}
 	set.attr({'text-anchor': ((horiz) ? 'start' : 'middle')});
 }
- 
- 
+
+
 function syndrom_chart(container, json, health_status, last_health_status) {
-	var 
+	var
 	margin_x = 160,
 	label_margin_x = 5, // Margin between bar & labels
-	w, paper, // Rapheal canvas 
+	w, paper, // Rapheal canvas
 	all_hbar, x, group_hbar, label_graph,
 	all_data, group, group_data, graph_data;
-	
+
 	all_data = prepare_data(json.data.syndrom, 'prop', last_health_status, health_status);
 	group = calc_user_group(user_profile, json.params.age_group);
 	if(group) {
@@ -155,16 +158,16 @@ function syndrom_chart(container, json, health_status, last_health_status) {
 	paper = Raphael(container, w, 240);
 	all_hbar = paper.hbarchart(margin_x, 0, 150, 200, all_data.values, { 'colors':all_data.colors });
 	create_labels(paper, all_hbar, true, all_data, margin_x, label_margin_x, true, (group ? false : true));
-	
+
 	paper.text(margin_x + 40, 220, i18n_labels['all_participants']);
-	
+
 	if(group) {
 		var x = margin_x + 150 + 50;
 		group_hbar = paper.hbarchart(x, 0, 150, 200, group_data.values, { 'colors':group_data.colors });
 		create_labels(paper, group_hbar, true, group_data, x, label_margin_x, false, true);
 		paper.text(x + 40, 220, group.label);
 	}
-	
+
 }
 
 var months = ['Jan','Fev','Mar','Avr','Mai','Jun','Jui','Aou','Sep','Oct','Nov','Dec'];
@@ -173,7 +176,7 @@ function StoryRaphael(history, users) {
 	var min = Number.MAX_VALUE; // Min timestamp
 	var max = 0;	// Max timestamp
 	var max_user_len = 0; // max length of username
-	
+
 	// Rebuild history by user, and extract stats
 	var hh = { };
 	var n = history.length;
@@ -189,7 +192,7 @@ function StoryRaphael(history, users) {
 	this.history = hh;
 	this.min_day = min;
 	this.max_day = max;
-	
+
 	var i = 0;
 	for(g in users) {
 		var n = users[g].length;
@@ -210,60 +213,60 @@ $.extend(StoryRaphael.prototype, {
 		'margin_bottom': 20 // Margin used to plot calendar date (date + month)
 	},
 	render: function(id, options) {
-		
+
 		options = options || {};
-		
+
 		$.extend(options, this.options);
-		
+
 		// Parameters
 		var dh = options.cell_h, // Height by user
-			dw = options.cell_w, // Width by day	
+			dw = options.cell_w, // Width by day
 			nb_users = this.nb_users,
 			nb_days = Math.ceil((this.max_day - this.min_day) / 86400), // Nb of days;
 			hh = this.history,
 			user_width = this.max_user_len * options.em + options.margin_left,
 			height = (nb_users) * dh + options.margin_bottom,
 			width = (nb_days + 1) * dw  + (user_width), // Width date cells + users names
-			paper, base_x; 
-		
+			paper, base_x;
+
 		console.log(id, width, height);
-		
+
 		var paper = Raphael(id, width, height);
-	 
+
 		var base_x = 0;
 		// draw grid
 		for(var i = 0; i <= nb_days; ++i) {
-			
+
 			// Draw date lines
 			x = base_x + dw * i;
 			var path = "M"+ x + ",0" + "L"+x+","+height;
 			var p = paper.path(path);
 			p.attr("stroke","#EEE");
-			
+
 			// Dates
 			var d = new Date();
 			d.setTime( (this.min_day + (i * 86400)) * 1000);
-			var a = {"font-size":"9px", "fill":"#AAA", 'stroke':'none' }; 
+			var a = {"font-size":"9px", "fill":"#AAA", 'stroke':'none' };
 			x = x + Math.round(dw/2);
 			var t = paper.text(x, height - 15, d.getDate() );
 			t.attr(a);
 			var t = paper.text(x, height - 5, months[d.getMonth()] );
 			t.attr(a);
 		}
-		
+
 		// Draw user lines and status
 		var attr_text = { "font-size": "10px", "text-anchor": "start" };
-		var attr_line = { "stroke": "#CCC", "stroke-width": 1};  
+		var attr_line = { "stroke": "#CCC", "stroke-width": 1};
 		var i = 0;
 		for(gid in this.users) {
 			var y = i * dh + Math.round(dh / 2);
 			var u = this.users[gid];
-			
+
 			// User name
 			var x = base_x + (nb_days + 1) * dw + 2;
 			var t = paper.text(x, y, u);
 			t.attr(attr_text);
-			
+
 			// User line
 			var x = base_x;
 			var l = (nb_days + 1) * dw;
@@ -298,6 +301,6 @@ $.extend(StoryRaphael.prototype, {
 
 });
 
- 
- 
+
+
 
